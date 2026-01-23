69cf1b59aa80e0b85eed15516d46a2b23c4e506c
AngLoong <angloong@outlook.com>
2026-01-23 18:01:31 +0800

    new file:   ALy/aliases_ALy.sh

---
+++ ALy/aliases_ALy.sh
@@ -0,0 +1,15 @@
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
+
+# --show-control-chars: help showing Korean or accented characters
+alias ggf='git fetch'
+alias ggs='git push'
+alias ggl='git pull'
+alias ggc='git commit'
+alias ggs='git status'
+alias ggd='git diff'
+alias gga='git add'
+alias ggo='git log'
+
+
+
