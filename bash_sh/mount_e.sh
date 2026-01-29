a3840530ebe20c6d3d7d3eb5d258e020d7c71e46
AngLoong <angloong@outlook.com>
2026-01-29 11:30:12 +0800

    调整目录

--- bash_sh/mount_e.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#https://jike.teracloud.jp/dav/
-#vdBZg7pLjtT3J9om
-
-mount -t ntfs-3g /dev/sda3 /mnt/ntfs/E/
-
-echo "ntfs-E mount successfully"
-
--- bash_sh/webdav.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#https://jike.teracloud.jp/dav/
-#vdBZg7pLjtT3J9om
-
-mount.davfs https://jike.teracloud.jp/dav/ /mnt/webdav
-
- echo "WebDAV server started"
