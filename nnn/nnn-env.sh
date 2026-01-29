6f09b4dcb141f51d60a1d26eb602a5c4de0de5f6
AngLoong <angloong@outlook.com>
2026-01-29 22:12:40 +0800

    新文件：   fish/config.fish-cachyos 	新文件：   fish/functions/fish_user_key_bindings.fish 	新文件：   fish/functions/n.fish 	新文件：   nnn/nnn-env.sh

---
+++ nnn/nnn-env.sh
@@ -0,0 +1,10 @@
+# nnn所有的配置都是通过环境变量设置的，所以只要将该文件source到相关文件即可，比如environment或bashrc或fish
+
+# 启动选项
+export NNN_OPTS='d'  # 显示隐藏文件，详细模式
+
+# 快捷书签
+export NNN_BMS='h:~/;w:~/Workspaces;n:~/Notes'
+
+# 加载插件（格式：插件名:快捷键）
+export NNN_PLUG='z:fzcd;o:fzopen;'
